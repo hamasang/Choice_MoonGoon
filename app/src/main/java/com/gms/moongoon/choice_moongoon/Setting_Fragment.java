96d578c29ff58fc76f2ee3a025bac4b0442d94c6
hamasang <cxa9809@gmail.com>
2015-10-17 16:47:14 +0900

    1

--- app/src/main/java/com/gms/moongoon/choice_moongoon/Setting_Fragment.java
+++ app/src/main/java/com/gms/moongoon/choice_moongoon/Setting_Fragment.java
@@ -1,35 +1,11 @@
 package com.gms.moongoon.choice_moongoon;
 
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.StrictMode;
-import android.os.SystemClock;
 import android.support.v4.app.Fragment;
-import android.telephony.PhoneStateListener;
-import android.telephony.TelephonyManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.Socket;
 
 public class Setting_Fragment extends Fragment {
 
@@ -38,16 +14,16 @@ public class Setting_Fragment extends Fragment {
     ImageView inside,outside;
     //셋팅창
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.setting, container, false);
         view.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
         return view;
 
-        ListView lv;
-
 
     }
+
 
     @Override
     public void onDestroyView() {
