7b2fa2d467f69b083095b02b31497c62bc894733
Hamasang <cxa9809@gmail.com>
2015-11-05 04:12:15 +0900

    init

---
+++ app/src/main/java/com/gms/moongoon/choice_moongoon/splash/rulletsplash.java
@@ -0,0 +1,34 @@
+package com.gms.moongoon.choice_moongoon.splash;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gms.moongoon.choice_moongoon.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by sang on 2015-11-05.
+ */
+public class rulletsplash extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(android.R.style.Theme_Holo_Light_NoActionBar_TranslucentDecor);
+        setContentView(R.layout.rulletsplash);
+        final Timer timer = new Timer();
+
+        final TimerTask myTask = new TimerTask() {
+            @Override
+            public void run() {
+//                Intent intent = new Intent(rulletsplash.this, SendActivity.class);
+//                startActivity(intent);
+                finish();
+            }
+        };
+        timer.schedule(myTask, 1300);
+
+    }
+}
